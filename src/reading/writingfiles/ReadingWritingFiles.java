958cce183b0e5c0a3dc28c2267fe3754942cc75a
Tyler Tackett <ttackett412@yahoo.com>
2017-11-15 14:13:15 -0500

    Committing the project with comments

--- src/reading/writingfiles/ReadingWritingFiles.java
+++ src/reading/writingfiles/ReadingWritingFiles.java
@@ -13,6 +13,7 @@ import java.util.Collections;
 
 public class ReadingWritingFiles {
 
+    //main method creates an ArryaList and calls the other methods
     public static void main(String[] args) {
 
         ArrayList<String> myNames = new ArrayList<>();
@@ -24,23 +25,28 @@ public class ReadingWritingFiles {
     
     public static void Reading(ArrayList<String> myNames)
     {
+        //finds the file through a file reader then gets put into a buffered reader for faster reading
         try(BufferedReader myReader = new BufferedReader(new FileReader(new File("names/BoyNames.txt"))))
         {
+            //reads the file while the lines read aren't null
             while(myReader.readLine() != null)
             {
                 myNames.add(myReader.readLine());
             }
         }
+        //catch : no more lines to read
         catch(EOFException eofe)
         {
             System.out.println("Out of lines to read");
             System.exit(0);
         }
+        //catch : couldn't find the file
         catch(IOException ioe)
         {
             System.out.println("Error finding file");
             System.exit(0);
         }
+        //catch : failsafe incase anyhitng else happens
         catch(Exception e)
         {
             System.out.println("other error");
@@ -50,20 +56,25 @@ public class ReadingWritingFiles {
     
     public static void Writing(ArrayList<String> myNames)
     {
+        //sorts the names
         Collections.sort(myNames);
         
+        //writes the names to through file writer with a bufferedwriter and a print writer
         try(PrintWriter myWriter = new PrintWriter(new BufferedWriter(new FileWriter("names/SortedNames.txt"))))
         {
+            //prints each name
             for(int i = 0; i < myNames.size(); i++)
             {
                 myWriter.println(myNames.get(i));
             }
         }
+        // catch : couldn't find file
         catch(IOException ioe)
         {
             System.out.println("Error finding file");
             System.exit(0);
         }
+        //catch : failsafe incase anyhitng else happens
         catch(Exception e)
         {
             System.out.println("other error");
